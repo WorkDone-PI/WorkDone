dc37a3acfded8d7f0eca03ff921f1794fa0f73e3
GABRIEL CHRISTOVAM LIMA DE MENEZES <0031432312028@lti.fatecso.edu.br>
2024-10-30 16:38:03 -0300

    Alterações nas telas de categorias

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,25 +1,32 @@
-@extends("layouts\app")
-
-@section("content")
-
-<div class="container">
-    <div class="align-self-center">
-        <h3 class="text-center mb-4">Cadastrar Categoria</h3>
-        <form action="{{ route('category.store') }}" method="post">
-            @csrf
-            <div class="mb-3">
-                <label for="name" class="form-label">Titulo</label>
-                <input type="text" class="form-control" title="title" id="name" value="{{ old('name') }}">
-                @error('name') {{ $message }} @enderror
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="shortcut icon" href="{{ asset('img/logoWK.png') }}" type="Favicon_Image_Location">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>WorkDone</title>
+</head>
+    <body>
+        <div class="container">
+            <div class="align-self-center">
+                <h3 class="text-center mb-4">Cadastrar Categoria</h3>
+                <form action="{{ route('category.store') }}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="Titulo" class="form-label">Titulo</label>
+                        <input type="text" class="form-control" title="Titulo" id="Titulo" value="{{ old('Titulo') }}">
+                        @error('Titulo') {{ $message }} @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="Descricao" class="form-label">Descrição</label>
+                        <input type="text" class="form-control" name="Descricao" id="Descricao" value="{{ old('Descricao') }}">
+                        @error('description') {{ $message }} @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">Cadastrar</button>
+                </form>
             </div>
-            <div class="mb-3">
-                <label for="description" class="form-label">Descrição</label>
-                <input type="text" class="form-control" name="description" id="description" value="{{ old('description') }}">
-                @error('description') {{ $message }} @enderror
-            </div>
-            <button type="submit" class="btn btn-primary w-100">Cadastrar</button>
-        </form>
-    </div>
-</div>
-
-@endsection
+        </div>
+    </body>
+</html>
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -5,8 +5,12 @@
 
 <div class="container">
     <div class="align-self-center">
-        <h3 class="text-center mb-4">{{ $category->name }}</h3>
-        <p>{{ $category->description }}</p>
+    @if ($categories)
+            <h3 class="text-center mb-4">{{ $categories->Titulo }}</h3>
+            <p>{{ $categories->Descricao }}</p>
+        @else
+            <h3 class="text-center mb-4">Não há nenhuma Categoria cadastrada</h3>
+        @endif
     </div>
 </div>
 
