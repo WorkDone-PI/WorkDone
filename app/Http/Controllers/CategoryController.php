bd3cb71e28d527873bb79f67e6f3f3528410ff0a
GABRIEL CHRISTOVAM LIMA DE MENEZES <0031432312028@lti.fatecso.edu.br>
2024-11-12 17:45:03 -0300

    Cadastro de Categorias

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,21 +10,29 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     function index(Request $request) {
-        $categories = Category::all();
+        $categories = Category::with('children')->whereNull('parent_id')->get();
+        return $categories;
+        $categories = Category::with('parent')->get();
         return view('categories.index', compact('categories'));
     }
     
     function create(Request $request) {
-        return view('categories.create');
+        $categories = Category::all();
+        return view('categories.create', compact('categories'));
     }
     
     function store(Request $request) {
         $request->validate([
-            'titulo' => 'required',
-            'descricao' => 'required'
+            'Titulo' => 'required',
+            'Descricao' => 'required',
+            'parent_id' => 'nullable|exists:categories,id'
         ]);
 
-        Category::create($request->all());
+        Category::create([
+            'Titulo' => $request->Titulo,
+            'Descricao' =>$request->Descricao,
+            'parent_id' => $request->parent_id
+        ]);
 
         return redirect()->route('category.index');
     }
@@ -35,14 +43,34 @@ class CategoryController extends Controller
     }
     
     function edit(Request $request) {
-        return "editar";
+        $categories = Category::findOrFail($id);
+        $categories = Category::where('id', '!=', $id)->get();
+
+        return view('categories.edit', compact('categories'));
     }
     
     function update(Request $request) {
-        return "atualizar";
+        $categories = Category::findOrFail($id);
+
+        $request->validate([
+            'Titulo' => 'required',
+            'Descricao' => 'required',
+            'parent_id' => 'nullable|exists:categories,id'
+        ]);
+
+        $categories->update([
+            'Titulo' => $request->Titulo,
+            'Descricao' => $request->Descricao,
+            'parent_id' => $request->parent_id
+        ]);
+
+        return redirect()->route('category.index');
     }
     
     function destroy(Request $request) {
-        return "remover";
+        $categories = Category::findOrFail($id);
+        $categories->delete();
+
+        return redirect()->route('category.index');
     }
 }
--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,7 +1,31 @@
-@extends("layouts\app")
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="shortcut icon" href="{{ asset('img/logoWK.png') }}" type="Favicon_Image_Location">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>WorkDone</title>
+</head>
+    <form action="{{ route('category.update', $category->id) }}" method="post">
+        @csrf
+        @method('PUT')
+        <h1> Editar Categorias </h1>
 
-@section("content")
+        <label for="Titulo">Nome</label>
+        <input type="text" name="Titulo" id="Titulo" value="{{ $category->Titulo }}"> <br />
 
-Editar Categorias
+        <label for="parent_id">Categoria pai</label>
+        <select name="parent_id" id="parent_id">
+            <option value=""></option>
+        @foreach ($categories as $parentCategory)
+            <option value="{{ $parentCategory->id }}" {{ $category->parent_id == $parentCategory->id ? 'selected' : ''}}>
+                {{ $parentCategory->Titulo }}
+            </option>
+        @endforeach
+        </select> <br />
 
-@endsection
+        <input type="submit" value="Salvar">
+    </form>
+</html>
